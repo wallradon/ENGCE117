23b65bb2f23ceff3632df2a58e10d1797b30831d
MIN <104954505+wallradon@users.noreply.github.com>
2024-11-25 15:03:06 +0700

    Add files via upload

---
+++ HW/HW03/hw03-1-codex/hw03-1-codex.c
@@ -0,0 +1,18 @@
+//TODO: #8 Arrange this C coding into CODEX pattern.
+#include <stdio.h>
+#include <conio.h>
+int main(){
+    int a = 10 ;
+    int b = 20, c = 30, d = 40 ; 
+    printf( "This is %d + %d = %d ", a, b, a + b ) ;
+    for(int i = 0 ; i < d - 3 ; i++ ) {
+        if(i < 30) {
+            printf( "Here " ) ;
+        if( i == 8 ) {
+            printf( "This is 8. " ) ;
+        }
+        }
+        printf( "ok" ) ;
+    }
+    return 0 ;
+}
---
+++ HW/HW03/hw03-2-codex-and-fix-bug/hw03-2-codex-and-fix-bug.cpp
@@ -0,0 +1,21 @@
+//TODO: #9 Fix bug and arrange code in CODEX pattern.
+#include <stdio.h>
+#include <conio.h>
+int main() {
+    float a, b = 3 ;
+    bool _1 = 0 ; 
+    printf( "%d ", 1 ) ; 
+    int i = 0 ;
+    if( 1!= 0 ) 
+    printf( "ok " ) ;
+    else    
+        while(i<=5) 
+            printf( " not-okey-%d ", ++i ) ;
+            for( i = 3 ; i < 10 ; i += 1 ){
+                if( i % 2 == ( !1?1:2 ) ) 
+                    printf("see see") ; 
+                    printf("haha") ;
+            } 
+                    
+return 0 ;
+}
