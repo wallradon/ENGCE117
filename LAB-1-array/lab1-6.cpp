efb36fe4aecda6a902283899cce43fcc49b3291e
MIN <104954505+wallradon@users.noreply.github.com>
2024-12-03 11:19:56 +0700

    Update lab1-6.cpp

--- LAB-1-array/lab1-6.cpp
+++ LAB-1-array/lab1-6.cpp
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
-int GetMatrix( int row, intcol ) ;
+int GetMatrix( int *row, int *col ) ;
 
 int main() {
-
-    int data, m, n, i, j ;
-
-    GetMatrix( &m, &n ) ;
-
-    data = new int[m n]; // จองหน่วยความจำสำหรับอาเรย์ 1 มิติ
-
+	
+	int *data, m, n, i, j ;		
+	
+	GetMatrix( &m, &n ) ;
+	
+	data = new int[m * n]; // จองหน่วยความจำสำหรับอาเรย์ 1 มิติ
+     
     // รับค่าจากผู้ใช้
     for (i = 0; i < m; i++) {
         for (j = 0; j < n; j++) {
@@ -17,23 +17,24 @@ int main() {
             scanf("%d", &data[i * n + j]); // เข้าถึงตำแหน่งในอาเรย์
         }
     }
-    printf("\nMatrix values:\n");
+	printf("\nMatrix values:\n");
     for (i = 0; i < m; i++) {
         for (j = 0; j < n; j++) {
             printf("%d ", data[i * n + j]); 
         }
         printf("\n"); // ขึ้นบรรทัดใหม่หลังจากพิมพ์ค่าของแต่ละแถว
     }
-
+    
     // ลบหน่วยความจำ
     delete[] data ;
-
-    return 0 ;
+	
+	return 0 ;
 }//end function
 
-int GetMatrix( int row, intcol ) {
-    // รับค่าจำนวนแถวและคอลัมน์
-        printf("How many rows, cols? ") ;
-        scanf("%d %d", row, col ) ;
-        return 0 ;
+int GetMatrix( int *row, int *col ) {
+	// รับค่าจำนวนแถวและคอลัมน์
+    	printf("How many rows, cols? ") ;
+    	scanf("%d %d", row, col ) ;
+		return 0 ;
 }//end function
+
