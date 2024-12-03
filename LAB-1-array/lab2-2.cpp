b28cf60ba72a47083bb98ed9297d8065e025cb7c
MIN <104954505+wallradon@users.noreply.github.com>
2024-12-04 00:49:30 +0700

    Create lab2-2.cpp

---
+++ LAB-1-array/lab2-2.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+
+char* reverse( char str1[] ) ;
+int main() {
+    char text[ 50 ] = "The quick  brown    fox" ;
+    char *out ;
+    out = reverse( text ) ;
+
+    printf( "\n %s \n", out ) ;
+
+    return 0 ;
+}//end function
+
+char* reverse( char str1[] ) {
+    char *out = new char[20] ;
+    int counts = strlen( str1 ) ;
+    for ( int i = 0 ; i < counts ; i++ )
+    {
+        out[i] = str1[counts - i - 1]  ;    
+    }//end for
+    out[counts] = '\0' ;
+    printf( "\n" ) ;
+    return out ;
+}//end function
