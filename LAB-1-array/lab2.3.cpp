573e2c6292a336525bb41ec81ffe5e78a7f45d6d
MIN <104954505+wallradon@users.noreply.github.com>
2024-12-04 16:17:00 +0700

    Update lab2.3.cpp

--- LAB-1-array/lab2.3.cpp
+++ LAB-1-array/lab2.3.cpp
@@ -1,46 +1,55 @@
+/*
+    #include <stdio.h>
+
+    void explode( char str1[], char splitter, char str2[][10], int *count ) ;
+
+    int main() {
+        char out[ 20 ][ 10 ] ;
+        int num ;
+        explode( "I/Love/You", "/" ) ;
+        return 0 ;
+    }//end function
+*/
 #include <stdio.h>
 #include <string.h>
 
-void explode(char str1[], char splitter, char str2[][10], int *count);
+void explode( char str1[], char splitter, char str2[][10], int *count ) ;
 
 int main() {
-    char out[20][10];  // อาร์เรย์เก็บคำที่แยกได้
-    int num;           // เก็บจำนวนคำที่แยกได้
+    char out[20][10] ;  // อาร์เรย์เก็บคำที่แยกได้
+    int num ;           // เก็บจำนวนคำที่แยกได้
+    
+    char text[] = "Hello World From Me" ;  
+    explode( text, ' ', out, &num ) ;
 
-    char text[] = "Hello World From Me";  // ใช้ตัวแปchar[ ] แทนค่าคงที่
-    explode(text, ' ', out, &num);
 
-    // แสดงผลลัพธ์
-    for (int i = 0; i < num; i++) {
-        printf("str2[%d] = \"%s\"\n", i, out[i]);
-    }
-    printf("count = %d\n", num);
+    for ( int i = 0; i < num; i++ ) {
+        printf( "str2[%d] = \"%s\"\n", i, out[i] ) ;
+    }//end for
+    printf( "count = %d\n", num ) ;
+    return 0 ;
+}//end function
 
-    return 0;
-}
+void explode( char str1[], char splitter, char str2[][10], int *count ) {
+    int index = 0, *count = 0, pos = 0 ;   
 
-void explode(char str1[], char splitter, char str2[][10], int *count) {
-    int index = 0;   // ตำแหน่งคำใน str2
-    int pos = 0;     // ตำแหน่งในคำปัจจุบันของ str2[index]
-
-    *count = 0;  // เริ่มต้นจำนวนคำที่แยกได้เป็น 0
-
-    for (int i = 0; str1[i] != '\0'; i++) {
-        if (str1[i] == splitter) {
-            if (pos > 0) {
-                str2[index][pos] = '\0';
-                index++;
-                pos = 0;
-            }
+    for ( int i = 0 ; str1[i] != '\0' ; i++ ) {//runค่าใน str1
+        if ( str1[i] == splitter ) {// ถ้าเจอ splitter
+            if ( pos > 0 ) {    
+                str2[index][pos] = '\0' ; 
+                index++ ; //ย้าย ช่อง 
+                pos = 0 ; 
+            }//end if
         } else {
-            str2[index][pos++] = str1[i];
-        }
-    }
+            str2[index][pos++] = str1[i] ;
+        }//end if
+    }//end for
 
     if (pos > 0) {
-        str2[index][pos] = '\0';
-        index++;
-    }
+        str2[index][pos] = '\0' ;
+        index++ ;
+    }//end if
 
-    *count = index;
-}
+    *count = index ;
+
+}//end function
