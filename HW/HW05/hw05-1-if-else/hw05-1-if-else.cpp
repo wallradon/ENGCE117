30c3e416b78d334e2572bf32d3f187e9b82a2618
MIN <104954505+wallradon@users.noreply.github.com>
2024-12-06 01:40:03 +0700

    Add files via upload

--- HW/HW05/hw05-1-if-else/hw05-1-if-else.cpp
+++ HW/HW05/hw05-1-if-else/hw05-1-if-else.cpp
@@ -1,1 +1,27 @@
+/*
+    รับตัวอักขระมาทั้งหมด 4 ค่า แล้วนำมาแสดงผลย้อนกลับ เช่น กรอกค่า L M X Y ต้องแสดงเป็น Y X M L เป็นต้น
+    
+    Test case:
+        L M Y K
+    Output:
+        Result: K Y M L
 
+    Test case:
+        A B C D
+    Output:
+        Result: D C B A
+*/
+
+#include<stdio.h>
+#include<string.h>
+
+int main() {
+    char ch1, ch2, ch3, ch4 ;
+    printf( "enter char :  " ) ;
+    scanf( "%c %c %c %c", &ch1, &ch2, &ch3, &ch4 ) ;
+    // int len = strlen(ch1) ;
+    // printf( "lch = %d\n", len) ;
+    if ( ch1 != NULL ) {
+        printf( "%c %c %c %c", ch4, ch3, ch2, ch1 ) ;
+    }
+}//end function
