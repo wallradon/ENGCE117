99e9c6c0590c3a6f9cf68fb4fda2a8377189a053
MIN <104954505+wallradon@users.noreply.github.com>
2025-01-23 11:15:33 +0700

    complete

--- 5_SingleLinklist/5-1.cpp
+++ 5_SingleLinklist/5-1.cpp
@@ -1,77 +1,89 @@
 #include <stdio.h>
-#include <string.h>
 
-struct studentNode {
-    char name[20] ;
-    int age ;
-    char sex ;
-    float gpa ;
-    int data ;
-    struct studentNode *next ;
-} ;
+struct Node {
+    int Data;
+    struct Node *Next;
+} typedef N;
 
-void ShowAll( struct studentNode *walk ) ;
-struct studentNode *AddNode( struct studentNode **walk, const char name[20], int age, char sex, float gpa ) ;
-struct studentNode *InsNode( struct studentNode **walk, const char name[20], int age, char sex, float gpa ) ;
-struct studentNode *DelNode( struct studentNode **walk ) ;
+void AddNode(N **, int Data);
+void InsertNode(N **, int Data, int position);
+void DeleteNode(N **, int position);
+void ShowNode(N **);
 
 int main() {
-    struct studentNode *start, *now ;
-    start = NULL ;
-    now = AddNode( &start, "one", 6, 'M', 3.11 ) ; 
-    ShowAll( start ) ;
-    now = AddNode( &start, "two", 8, 'F', 3.22 ) ; 
-    ShowAll( start ) ;
-    now = InsNode( &now, "three", 10, 'M', 3.33 ) ; 
-    ShowAll( start ) ;
-    now = InsNode( &now, "four", 12, 'F', 3.44 ) ; 
-    ShowAll( start ) ;
-    DelNode( &now ) ; 
-    ShowAll( start ) ;
+    N *start = NULL;
+    int data = 0, pos = 0 ;
+    printf( "Add (data) :" ) ; scanf( "%d", &data ) ; AddNode(&start, data);
+    printf( "Add (data) :" ) ; scanf( "%d", &data ) ; AddNode(&start, data);
+    printf( "Add (data) :" ) ; scanf( "%d", &data ) ; AddNode(&start, data);
+    ShowNode(&start);
+
+    printf( "Insert (data, pos) :" ) ; scanf( "%d %d", &data, &pos ) ; InsertNode(&start, data, pos);
+    printf( "Insert (data, pos) :" ) ; scanf( "%d %d", &data, &pos ) ; InsertNode(&start, data, pos);
+    printf( "Insert (data, pos) :" ) ; scanf( "%d %d", &data, &pos ) ; InsertNode(&start, data, pos);
+    ShowNode(&start);
+
+    printf( "Delete (pos) :" ) ; scanf( "%d", &pos ) ; DeleteNode(&start, pos);
+    printf( "Delete (pos) :" ) ; scanf( "%d", &pos ) ; DeleteNode(&start, pos);
+    printf( "Delete (pos) :" ) ; scanf( "%d", &pos ) ; DeleteNode(&start, pos);
+    ShowNode(&start);
+
     return 0 ;
-}//end function
+}
 
-void ShowAll( struct studentNode *walk ) {
-    while( walk != NULL ) {
-        printf( "%s ", walk->name ) ;
-        walk = walk->next ;
-    }//end while
-    printf( " " ) ;
-    printf( "\n" ) ;
-}//end function
-
-struct studentNode *AddNode( struct studentNode **walk, const char name[20], int age, char sex, float gpa ) {
-    
-    while ( *walk != NULL ) {
-        walk = &(*walk)->next ;
-    }//end walk
-
-    *walk = new struct studentNode ;
-    strcpy( ( *walk )->name, name ) ;
-    ( *walk )->age = age ;
-    ( *walk )->sex = sex ;
-    ( *walk )->gpa = gpa ;
-    ( *walk )->next = NULL ;
-    return *walk ;
+void AddNode(N **Walk, int Data) {
+     while ( *Walk != NULL ) {
+        Walk = &(*Walk)->Next ;
+    }//end Walk
+    *Walk = new N ;
+    ( *Walk )->Data = Data ;
+    ( *Walk )->Next = NULL ;
 }
 
 
-struct studentNode *InsNode( struct studentNode **walk, const char name[20], int age, char sex, float gpa ) {
-    struct studentNode *temp ;
-    temp = (*walk)->next ;
-    
-    (*walk)->next = new struct studentNode ;
-    strcpy( ( *walk )->next->name, name ) ; 
-    ( *walk )->next->age = age ;
-    ( *walk )->next->sex = sex ;
-    ( *walk )->next->gpa = gpa ;
-    (*walk)->next->next = temp ;  
+void ShowNode(N **Walk) {
+    N *showstart = *Walk ;
+    // printf("test%d \n", ( showstart )->Next->Data) ;
+    while ( showstart != NULL ) {
+        printf("%d ", ( showstart )->Data) ;
+        showstart = ( showstart )->Next ;
+    }
+    printf("\n") ;
+}
 
-    return (*walk)->next ;
+void InsertNode(N **Walk, int Data, int position) {
+    N *temp0 = new N ;
+    temp0->Data = Data ; //เลขใหม่
 
-}//end function
+    if ( position == 1 ) {
+        temp0->Next = *Walk ;
+        *Walk = temp0 ;
+    }else if ( position > 1 ) {
 
-struct studentNode *DelNode( struct studentNode **walk ) {
-    ( *walk ) = NULL ;
-    return (*walk) ;
-}//end function
+        N *temp1 = *Walk ;
+        for ( int i = 1 ; i < position - 1 && temp1 != NULL ; i++ ) {
+            temp1 = temp1 -> Next ;
+        }//end for position
+        temp0->Next = temp1->Next ;
+        temp1->Next = temp0 ;
+        // printf("test2 %d \n", ( temp1 )->Next->Data) ;
+
+    }//end if position 1
+}
+
+void DeleteNode(N **Walk, int position) {
+    N *tempDel1 = *Walk ;
+    if ( position == 1 ) {
+        *Walk = tempDel1->Next ;
+        // printf("test2 %d \n", ( *Walk )->Next->Data) ;
+        delete tempDel1 ;
+    }else if ( position > 1 ) {
+        N *tempDel2 = NULL ;
+        for ( int i = 1 ; i < position && tempDel1 != NULL ; i++ ){
+            tempDel2 = tempDel1 ;
+            tempDel1 = tempDel1->Next ;
+        }//end for
+        tempDel2->Next = tempDel1->Next ;
+        delete tempDel1 ;
+    }
+}
