9f3e04cdf1903e34598400ea1d690fdfc7595a49
MIN <104954505+wallradon@users.noreply.github.com>
2025-02-23 21:47:28 +0700

    Update hw06-2-roman-number.cpp

--- HW/HW06/hw06-2-roman-number/hw06-2-roman-number.cpp
+++ HW/HW06/hw06-2-roman-number/hw06-2-roman-number.cpp
@@ -36,7 +36,7 @@ int main() {
         int values[13] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1} ;
         const char* symbols[13] = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"} ;
 
-        for ( int i = 0; i < 13; i++ ) {
+        for ( int i = 0 ; i < 13 ; i++ ) {
             while ( num >= values[i] ) { 
                 printf( "%s", symbols[i] ) ;
                 num -= values[i] ; 
